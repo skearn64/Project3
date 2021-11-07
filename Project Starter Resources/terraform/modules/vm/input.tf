2d0bf8ed7054d3c79f350bb6f30d303360d407cc
skearn64 <84685958+skearn64@users.noreply.github.com>
2021-11-07 17:32:52 +0000

    Add files via upload

---
+++ Project Starter Resources/terraform/modules/vm/input.tf
@@ -0,0 +1,9 @@
+# Resource Group
+variable resource_group {}
+variable location {}
+# VM
+variable "application_type" {}
+variable "resource_type" {}
+variable username {}
+variable "public_ip_address" {}
+variable "subnet_id" {}
